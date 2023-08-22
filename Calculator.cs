b5f1e26b9ba56f40737dffb9cbb0d166b28a5b61
Degamie_sign <106448560+degamie@users.noreply.github.com>
2023-08-22 15:29:14 +0530

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace Calculator
+{
+    class Calculator_program
+    {
+        static void Main(String[] args)
+        {
+            //variable declaration & initialization
+
+            double num1 = 0, num2 = 0, result = 0;
+            Console.WriteLine("Welcome to Calcultor!");
+
+            //input
+            Console.WriteLine("Enter Num1");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Num2");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            //Option Selection
+            Console.WriteLine("Select an Option\n");
+            Console.WriteLine("\t+: Addition:");
+            Console.WriteLine("\t-: Subtraction:");
+            Console.WriteLine("\t*: Multiplication:");
+            Console.WriteLine("\t/: Division:");
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    result = num1 + num2;
+                    //Console.Write($"result:{num1}+{num2}="+result);
+                    Console.WriteLine($"result:" + result);
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    //Console.Write($"result:{num1}-{num2}"+result);
+                    Console.WriteLine($"result:" + result);
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    //Console.Write($"result:{num1} + {num2}",+result);
+                    Console.WriteLine($"result:" + result);
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    //Console.Write($"result:{num1}/{num2}",+result);
+                    Console.WriteLine($"result:" + result);
+                    break;
+                default:
+                    Console.Write("Invalid Option:\n");
+                    break;
+                    //Console.ReadKey();
+
+            }
+        }
+      
+    }
+}
